a781def558793fb3a3b608387031ae43b09a05da
maisieb01 <31147730+maisieb01@users.noreply.github.com>
2017-08-23 21:18:06 +1000

    Update twitter_data_access.py

--- twitter_data_access.py
+++ twitter_data_access.py
@@ -1,5 +1,5 @@
 from tweepy import OAuthHandler
-
+from tweepy.streeming import StreemListener 
 Consumer_Key ='whNUhg5NcrmJyTRa6dSipOMq2'
 Consumer_Secret='eACzXRYOySCouGzMDfbKI7d1lFNyavXagmmxD1tY6gf8XVhLZP'
 Access_Token='2677399741-UJrzodzbUn1HoEobo6T9mSq2tWxnmP37otu8MMw'
@@ -7,7 +7,7 @@ Access_TokenSecret='kLe3XbfjKV9uDdgp1iB08XqIkbEeMyUmrIjzRivvyF7RF'
 
 auth= OAuthHandler (Consumer_Key , Consumer_Secret)
       OAuthHandler class : tweepy.OAuthHandler 
-auth.           
+auth.set_access_token(access_token,Access_TokenSecret)   
     
       
 
