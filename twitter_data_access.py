960c217c00f78a27178c5a68ee41a98ba0909ce9
maisieb01 <31147730+maisieb01@users.noreply.github.com>
2017-08-23 20:58:36 +1000

    Update twitter_data_access.py

--- twitter_data_access.py
+++ twitter_data_access.py
@@ -4,8 +4,10 @@ Consumer_Key ='whNUhg5NcrmJyTRa6dSipOMq2'
 Consumer_Secret='eACzXRYOySCouGzMDfbKI7d1lFNyavXagmmxD1tY6gf8XVhLZP'
 Access_Token='2677399741-UJrzodzbUn1HoEobo6T9mSq2tWxnmP37otu8MMw'
 Access_TokenSecret='kLe3XbfjKV9uDdgp1iB08XqIkbEeMyUmrIjzRivvyF7RF'
+
 auth= OAuthHandler (Consumer_Key , Consumer_Secret)
       OAuthHandler class : tweepy.OAuthHandler 
+auth.           
     
       
 
